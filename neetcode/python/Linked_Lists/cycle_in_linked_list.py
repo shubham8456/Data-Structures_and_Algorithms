35f332bfb2091ae11ca4d9f0a6d58d00e3719f45
Shubham Rawat <shubham84567@gmail.com>
2025-10-01 17:02:11 +0530

    more linked-list related problems

---
+++ neetcode/python/Linked_Lists/cycle_in_linked_list.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# O(1) space complexity, O(n) time complexity
+# Floyd's tortoise and hare algo
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+# O(n) space complexity, O(n) time complexity
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         hash_set = set()
+#         while head:
+#             if head in hash_set:
+#                 return True
+#             hash_set.add(head)
+#             head = head.next
+#         return False
---
+++ neetcode/python/Linked_Lists/merge_sorted_linked_lists.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+class ListNode:
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+
+        return dummy.next
+
+
+
+# Usage
+
+def print_node(head: ListNode):
+  while head:
+    print(head.val)
+    head = head.next
+
+l1_3 = ListNode(4, None)
+l1_2 = ListNode(2, l1_3)
+l1_1 = ListNode(1, l1_2)
+
+l2_3 = ListNode(5, None)
+l2_2 = ListNode(3, l2_3)
+l2_1 = ListNode(1, l2_2)
+
+result = Solution().mergeTwoLists(l1_1, l2_1)
+print_node(result)
